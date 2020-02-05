d8a77c22f433d1a10244b6fe7dda930f712972a0
kedoubitcoin <kedoubitcoin@126.com>
2020-02-05 14:36:36 +0800

    Delete some comment lines.

--- app/sys_battery.c
+++ app/sys_battery.c
@@ -140,23 +140,6 @@ void bas_init(void)
 {
     uint32_t       err_code;
 	ble_bas_init_t     bas_init;
-	// Initialize Battery Service.
-#if 0	
-	ble_bas_init_t bas_init_obj;
-
-    memset(&bas_init_obj, 0, sizeof(bas_init_obj));	
-
-    bas_init_obj.evt_handler          = NULL;//on_bas_evt;
-    bas_init_obj.support_notification = true;
-    bas_init_obj.p_report_ref         = NULL;
-    bas_init_obj.initial_batt_level   = 100;
-	
-
-	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&bas_init_obj.battery_level_char_attr_md.cccd_write_perm);
-	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&bas_init_obj.battery_level_char_attr_md.read_perm);
-	BLE_GAP_CONN_SEC_MODE_SET_NO_ACCESS(&bas_init_obj.battery_level_char_attr_md.write_perm);
-	BLE_GAP_CONN_SEC_MODE_SET_OPEN(&bas_init_obj.battery_level_report_read_perm);
-#endif	
 	
     // Here the sec level for the Battery Service can be changed/increased.
     bas_init.bl_rd_sec        = SEC_OPEN;
