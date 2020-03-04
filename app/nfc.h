2dfa8a07aeb848a033fd73d76e950557de47fb65
kedoubitcoin <kedoubitcoin@126.com>
2020-03-04 12:11:21 +0800

    Add function declaration.

--- app/nfc.h
+++ app/nfc.h
@@ -11,6 +11,9 @@ extern bool data_recived_flag;
 extern uint8_t data_recived_buf[1024];
 extern uint32_t data_recived_len;
 
+bool i2c_master_write(uint8_t *buf,uint32_t len);
+bool i2c_master_read(void);
+
 int nfc_init(void);
 int twi_master_init(void);
 
