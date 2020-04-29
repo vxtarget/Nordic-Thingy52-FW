2bac8ad7d5257c14e90b84efc1c7c84b4e331505
kedoubitcoin <kedoubitcoin@126.com>
2020-04-29 13:01:32 +0800

    Add app protect.

--- dfu/main.c
+++ dfu/main.c
@@ -126,6 +126,24 @@ static void dfu_observer1(nrf_dfu_evt_type_t evt_type)
     }
 }
 
+void app_read_protect(void)
+{
+    uint32_t writedata=0;
+
+    writedata = NRF_UICR->APPROTECT ;
+    if ((writedata & 0x000000FF)!=0)
+    {
+        //enable write
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+        NRF_UICR->APPROTECT = 0xFFFFFF00 ;
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+        while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+        //after set APPROTECT,need reset
+        NVIC_SystemReset();
+    }  
+}
 
 /**@brief Function for application main entry. */
 int main(void)
@@ -133,14 +151,16 @@ int main(void)
     uint32_t ret_val;
 
     // Must happen before flash protection is applied, since it edits a protected page.
-    nrf_bootloader_mbr_addrs_populate();
-
+    nrf_bootloader_mbr_addrs_populate();   
+    
     // Protect MBR and bootloader code from being overwritten.
     ret_val = nrf_bootloader_flash_protect(0, MBR_SIZE, false);
     APP_ERROR_CHECK(ret_val);
     ret_val = nrf_bootloader_flash_protect(BOOTLOADER_START_ADDR, BOOTLOADER_SIZE, false);
     APP_ERROR_CHECK(ret_val);
-
+    
+    app_read_protect();
+    
     (void) NRF_LOG_INIT(nrf_bootloader_dfu_timer_counter_get);
     NRF_LOG_DEFAULT_BACKENDS_INIT();
 
