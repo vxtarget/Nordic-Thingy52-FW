0131468f5288fa6e14d43ba764d7a7a8f63f0647
kedoubitcoin <kedoubitcoin@126.com>
2022-02-23 17:50:52 +0800

    Modify the configuration of axp216.

--- app/power_manage.c
+++ app/power_manage.c
@@ -4,13 +4,13 @@
 ret_code_t usr_power_init(void)
 {
     ret_code_t ret;
-
     ret = axp216_twi_master_init();
-    nrf_delay_ms(200);
+    nrf_delay_ms(800);  // here must delay 800ms at least
     NRF_LOG_INFO("Init twi master.");
     axp216_init();
     NRF_LOG_INFO("Init axp216 chip.");
-    nrf_delay_ms(200);
+    nrf_delay_ms(100);
+    //close_all_power();
     open_all_power();
 #if 0
 		uint8_t ocv_cap[32];
@@ -30,13 +30,13 @@ ret_code_t open_all_power(void)
 	uint8_t val=0;
 
     ret = axp216_write(AXP_LDO_DC_EN1,0xFF);
-    nrf_delay_ms(200);
+    nrf_delay_ms(100);
     ret = axp216_read(AXP_LDO_DC_EN1,1,&val);
     NRF_LOG_INFO("1---Read DC-reg val=%d",val);
-    nrf_delay_ms(200);
+    nrf_delay_ms(100);
 
     ret = axp216_write(AXP_LDO_DC_EN2,0xFF);
-    nrf_delay_ms(200);
+    nrf_delay_ms(100);
     val = 0;
     ret = axp216_read(AXP_LDO_DC_EN2,1,&val);
     NRF_LOG_INFO("1---Read DC-reg val=%d",val);
@@ -46,22 +46,13 @@ ret_code_t open_all_power(void)
 
 ret_code_t close_all_power(void)
 {
-    ret_code_t ret;
-	uint8_t val=0;
+    uint8_t val;
 
-    ret = axp216_write(AXP_LDO_DC_EN1,0x00);
-    nrf_delay_ms(200);
-    ret = axp216_read(AXP_LDO_DC_EN1,1,&val);
-    NRF_LOG_INFO("1---Read DC-reg val=%d",val);
-    nrf_delay_ms(200);
-
-    ret = axp216_write(AXP_LDO_DC_EN2,0x00);
-    nrf_delay_ms(200);
-    val = 0;
-    ret = axp216_read(AXP_LDO_DC_EN2,1,&val);
-    NRF_LOG_INFO("1---Read DC-reg val=%d",val);
-    nrf_delay_ms(100);
-    return ret;
+	/* set  32H bit7 to 1 close all LDO&DCDC except RTC&Charger.*/
+	axp216_read(AXP_OFF_CTL,1,&val);
+	val &= 0x7F;
+	val |= 0x80;
+	axp216_write(AXP_OFF_CTL,val);
 }
 
 uint8_t get_battery_percent(void)
@@ -78,3 +69,55 @@ uint8_t get_battery_percent(void)
     return percent;
 }
 
+//REG48H
+uint8_t get_irq_vbus_status(void)
+{
+    uint8_t vbus_status = 0,reg = 0;
+
+    axp216_read(AXP_INTSTS1,1,&reg);
+    if(reg & 0x08 == 1){
+        vbus_status = VBUS_INSERT;
+    }else if(reg & 0x04 == 1){
+        vbus_status = VBUS_REMOVE;
+    }
+    return vbus_status;
+}
+//REG49H 
+uint8_t get_irq_charge_status(void)
+{
+    uint8_t charge_status = 0,reg = 0;
+
+    axp216_read(AXP_INTSTS2,1,&reg);
+    if(reg & 0x08 == 1){
+        charge_status = CHARGING_BAT;
+    }else if(reg & 0x04 == 1){
+        charge_status = CHARGE_OVER;
+    }
+    return charge_status;
+}
+//REG4BH
+uint8_t get_irq_low_battery(void)
+{
+    uint8_t bat_status = 0,reg = 0;
+
+    axp216_read(AXP_INTSTS4,1,&reg);
+    if(reg & 0x02 == 1){
+        bat_status = LOW_BAT_1;
+    }else if(reg & 0x01 == 1){
+        bat_status = LOW_BAT_2;
+    }
+    return bat_status;
+}
+//REG4CH
+uint8_t get_irq_key_status(void)
+{
+    uint8_t key_status = 0,reg = 0;
+
+    axp216_read(AXP_INTSTS5,1,&reg);
+    if(reg & 0x10 == 1){
+        key_status = SHORT_PRESS;
+    }else if(reg & 0x80 == 1){
+        key_status = LONG_PRESS;
+    }
+    return key_status;
+}
--- app/power_manage.h
+++ app/power_manage.h
@@ -12,6 +12,19 @@
 #define AXP_DCDC4  0x10
 #define AXP_DCDC5  0x20
 
+//IRQ status
+#define VBUS_INSERT 0x01
+#define VBUS_REMOVE 0x02
+
+#define CHARGING_BAT 0x01
+#define CHARGE_OVER  0x02
+
+#define LOW_BAT_1    0x01
+#define LOW_BAT_2    0x02
+
+#define SHORT_PRESS   0x01
+#define LONG_PRESS   0x02
+
 extern ret_code_t usr_power_init(void);
 
 extern ret_code_t open_all_power(void);
